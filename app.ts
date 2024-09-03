6938a971601b5b9bad6259801dde473cb1e2ce21
mohamed fathi <Mkhalifaali>
2024-09-03 06:32:51 +0300

    categories & subcategories

--- app.ts
+++ app.ts
@@ -1,13 +1,26 @@
-const express = require('express');
-const app = express();
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'; 
 
-app.get('/', (request, response) => {
+const app:express.Application = express();
+
+dotenv.config();
+
+
+mongoose.connect('mongodb://localhost:27017/e-commerce')
+.then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.error("Error connecting to MongoDB: ", err);    
+})
+
+app.get('/', (request : express.Request, response :express.Response) => {
     response.send("Welcome to home");
 });
 
-app.get('/hello', (request, response) => {
-    response.send("Hello World");
-});
+// app.get('/hello', (request : Request, response : Response) => {
+//     response.send("Hello World");
+// });
 
 app.listen(3000, () => {
     console.log("Started on port 3000");
